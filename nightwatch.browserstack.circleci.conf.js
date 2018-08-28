8585d81eb4b588172b0a5f82542db419e1da3755
Christophe Hurpeau <christophe@hurpeau.com>
2018-08-28 19:38:35 +0200

    fix

--- nightwatch.browserstack.circleci.conf.js
+++ nightwatch.browserstack.circleci.conf.js
@@ -1,20 +1,17 @@
 module.exports = (function (config) {
-  console.log(process.env.BROWSERSTACK_USER);
   // "output_folder": "${CIRCLE_TEST_REPORTS}"
 
   config.selenium.start_process = false;
   config.selenium.host = 'hub-cloud.browserstack.com';
   config.selenium.port = 80;
 
-  config.browserstack = {
+  config.test_settings.default = {
     "desiredCapabilities": {
       "browserstack.user": process.env.BROWSERSTACK_USER,
       "browserstack.key": process.env.BROWSERSTACK_KEY,
       "browser": "ie"
-    }
-  };
+    },
 
-  config.test_settings.default = {
     selenium_host: config.selenium.host,
     selenium_port: config.selenium.port,
   };
